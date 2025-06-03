c8ff9d680332eb5434c40022ec5401541cdf2b69
anaswara <anaswara.n@cdac.in>
2025-06-03 18:07:46 +0530

    expected value updated

--- src/modules/ims_auth/authorize.c
+++ src/modules/ims_auth/authorize.c
@@ -1104,6 +1104,7 @@ int ims_authenticate(struct sip_msg *msg, str *prealm, int is_proxy_auth)
 					response16.len, response16.s, 32, expected, 32, ha1,
 					msg->first_line.u.request.method.len,
 					msg->first_line.u.request.method.s);
+			strncpy(response16.s,expected,response16.len);
 			break;
 		default:
 			LM_ERR("algorithm %.*s is not handled.\n",
@@ -1115,7 +1116,7 @@ int ims_authenticate(struct sip_msg *msg, str *prealm, int is_proxy_auth)
 	expires = cscf_get_max_expires(msg, 0);
 
 	if(response16.len == expected_len
-			&& strncasecmp(response16.s, response16.s, response16.len) == 0) {
+			&& strncasecmp(response16.s, expected, response16.len) == 0) {
 		if(max_nonce_reuse > 0 && av->status == AUTH_VECTOR_SENT) {
 			/* first use of a reusable vector */
 			/* set the vector's new timeout */
